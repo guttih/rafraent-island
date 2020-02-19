1c2208cb26af267c41795ceb96c76baf1bdf8fc1
Guðjón Hólm Sigurðsson <gudjon.holm.sigurdsson@advania.is>
2020-02-19 16:14:46 +0000

    fixing folder structure nodesoap

--- vinnumalastofnun/nodesoap/build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var App_1 = require("./App");
-var PORT = 8000;
-App_1.default.listen(PORT, function () {
-    console.info("Express server listening on http://localhost:" + PORT);
-});
-//# sourceMappingURL=index.js.map
